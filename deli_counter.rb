3a8ddef78e9ea51be632c8a83e2aace548b578e9
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-03-28 17:29:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,11 @@
 # Write your code here.
 katz_deli = []
 
-def line(deli)
+def line(katz_deli)
   if katz_deli.length > 0
     count = 1
     deli_line = []
-    deli.each do |i|
+    katz_deli.each do |i|
       deli_line.push(count.to_s + ". " + i +" ")
       count+=1
     end
