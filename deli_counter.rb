1febb1498bbd453696ecd9dcf62727d05082e4d4
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-03-28 17:41:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,7 +23,7 @@ end
 def now_serving(katz_deli)
   if katz_deli.length > 0
     current = katz_deli.pop
-    puts "Currently serving "
+    puts "Currently serving #{current}"
   else
     puts "There is nobody waiting to be served!"
   end
