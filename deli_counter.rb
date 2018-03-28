00c225d95685ad838608109965620ec9eb7de0d9
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-03-28 17:21:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,11 @@ katz_deli = []
 
 def line(katz_deli)
   if katz_deli.length > 0
+    count = 1
+    katz_deli.each do |i|
+      puts "The line is currently: " + count + ". " + i
+      count+=1
+    end
   else
     puts "The line is currently empty."
   end
